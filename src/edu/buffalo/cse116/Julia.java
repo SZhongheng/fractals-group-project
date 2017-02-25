1a91b1448b37d8b3b75c875b437fdc543389aed3
zhongheng <sunzh95@gmail.com>
2017-02-24 21:04:14 -0500

    Julia set

---
+++ src/edu/buffalo/cse116/Julia.java
@@ -0,0 +1,64 @@
+package edu.buffalo.cse116;
+
+
+/*The fractals your program will be generating this term are all "escape-time algorithms". Escape-time algorithms imagine the picture is a Cartesian plane (I had to look this up, too -- it means point is defined by its x- and y-coordinates). Each PIXEL (point on this plane) is then colored using the following process:
+
+    Set xCalc = current point's x-coordinate
+    Set yCalc = current point's y-coordinate
+    Set dist = distance from the point (xCalc, yCalc) to (0,0) using the Pythagorean theorem
+    Set passes = 0
+    While dist ≤ 2 and passes < 255 do
+      Update xCalc and yCalc using the update function defined for the fractal being generated
+      Increase passes by 1
+      Set dist = distance from the point (xCalc, yCalc) to (0,0) using the Pythagorean theorem
+    EndWhile
+    Set the current point's escape-time equal to passes*/
+    
+public class Julia{
+	
+	private int _array1[][] = new int [512][512];
+	
+	private double _x = 3.4/512;
+	private double _y = 2.0/512;
+	  
+
+	
+	
+	public Julia(){
+	 for (int i = 0; i < 511; i ++){
+		 for (int j = 0; i < 511; i ++){
+			 double xCalc = translateX(i);
+			 double yCalc = translateY(j);
+			 _array1[i][j] = EscapeTimeAlgorithm(xCalc, yCalc);
+		 }
+	 }
+		 
+			 
+		 }
+	 public double translateX(int i){
+		 double xCalc = -1.7 + _x * i;
+		 return xCalc;
+		 
+	 }
+	 public double translateY(int j){
+		 double yCalc = -1.0 + _y * j;
+		return yCalc;
+	 }
+		
+	
+		
+	public int EscapeTimeAlgorithm(double xCalc, double yCalc){
+		int passes = 0;
+		double dist = Math.sqrt(xCalc * xCalc + (yCalc * yCalc));
+		while (dist <= 2 && passes < 255){
+			xCalc = (xCalc * xCalc) - (yCalc * yCalc) + -0.72689;
+			yCalc = (2 * xCalc * yCalc) + 0.188887;
+			passes ++;
+			dist = Math.sqrt(xCalc * xCalc + yCalc * yCalc);
+		}
+		return passes;
+	}
+			
+		
+		
+}
---
+++ src/edu/buffalo/cse116/JuliaTest.java
@@ -0,0 +1,29 @@
+package edu.buffalo.cse116;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class JuliaTest {
+	
+	Julia j = new Julia();
+	
+	
+	@Test
+	public void testJuliaNeverExceed(){
+		assertEquals(255, j.EscapeTimeAlgorithm(1.0492187499999897, -0.234375), 0.000001);
+	}
+	@Test
+	public void testJuliaExceed(){
+		assertEquals(1, j.EscapeTimeAlgorithm(1.6933593749999853, 0.9765625), 0.0000001);
+	}
+	@Test
+	public void xTranslate(){
+		assertEquals(1.7, j.translateX(512), 0.001);
+	}
+	@Test
+	public void yTranslate(){
+		assertEquals(1.0, j.translateY(512),0.001);
+	}
+
+}
