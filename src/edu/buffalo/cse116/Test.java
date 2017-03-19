cf183a5527d04bb91d84371d48ea154061812bf6
Sunny <Sunny@DESKTOP-4REGNHL>
2017-03-18 19:44:22 -0700

    Made all necessary updates. Added the text files. Phase 2 complete.

--- src/edu/buffalo/cse116/Test.java
+++ src/edu/buffalo/cse116/Test.java
@@ -23,9 +23,11 @@ import java.awt.Color;
 import javax.swing.JOptionPane;
 import javax.swing.JButton;
 import java.awt.GridLayout;
+import java.awt.Window;
+
 //d
 public class Test {
-	
+
 	FractalPanel FP = new FractalPanel();
 	Fractal F = new Fractal();
 	private JFrame frame;
@@ -61,18 +63,52 @@ public class Test {
 	 */
 	private void initialize() {
 		frame = new JFrame();
-		frame.setBounds(0, 0, 450, 300);
+		frame.setBounds(0, 0, 960, 540);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		
+
 		JMenuBar menuBar = new JMenuBar();
 		menuBar.setAlignmentX(Component.LEFT_ALIGNMENT);
-		menuBar.setFont(new Font("Segoe UI", Font.PLAIN, 72));
+		menuBar.setFont(menuBar.getFont().deriveFont(menuBar.getFont().getSize() + 10f));
 		frame.setJMenuBar(menuBar);
-		
+
 		JMenu mnFile = new JMenu("File");
+		mnFile.setFont(new Font("Tahoma", Font.PLAIN, 30));
 		menuBar.add(mnFile);
 		
+		JMenuItem mnESC = new JMenuItem("Set Escape Distance");
+		mnESC.setFont(new Font("Tahoma", Font.PLAIN, 30));
+		mnFile.add(mnESC);
+		mnESC.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				try {
+					numInput = Double.parseDouble(JOptionPane.showInputDialog(frame, "Enter a numberical value greater than or equal to 0: ", "Example: 2 or 3.7"));
+					if (numInput < 0) {
+						throw new NumberFormatException();
+					}
+					if (x == 1) {
+						FP.updateImage(F.bsArray(numInput));
+					}
+					if (x == 2) {
+						FP.updateImage(F.jArray(numInput));
+					}
+					if (x == 3) {
+						FP.updateImage(F.maArray(numInput));
+					}
+					if (x == 4) {
+						FP.updateImage(F.muArray(numInput));
+					}
+				} catch (NumberFormatException n) {
+					String[] buttons = { "OK" };
+					JOptionPane.showOptionDialog(frame, "Please enter a numerical value greater than or equal to 0!", "ERROR", JOptionPane.WARNING_MESSAGE, 0,
+							null, buttons, buttons[0]);
+				} catch (NullPointerException npe) {
+
+				}
+			}
+		});
+
 		JMenuItem mntmExit = new JMenuItem("Exit");
+		mntmExit.setFont(new Font("Tahoma", Font.PLAIN, 30));
 		mntmExit.addMouseListener(new MouseAdapter() {
 			@Override
 			public void mouseClicked(MouseEvent arg0) {
@@ -85,190 +121,203 @@ public class Test {
 			}
 		});
 		mnFile.add(mntmExit);
-		
+
 		JMenu mnFractal = new JMenu("Fractal");
+		mnFractal.setFont(new Font("Tahoma", Font.PLAIN, 30));
 		menuBar.add(mnFractal);
-		
+
 		JMenuItem mntmBurningShip = new JMenuItem("Burning Ship");
+		mntmBurningShip.setFont(new Font("Tahoma", Font.PLAIN, 30));
 		mnFractal.add(mntmBurningShip);
-		mntmBurningShip.addActionListener(new ActionListener(){
-			public void actionPerformed(ActionEvent e){
+		mntmBurningShip.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
 				FP.updateImage(F.bsArray(numInput));
-				x=1;
+				x = 1;
 			}
 		});
 		JMenuItem mntmJulia = new JMenuItem("Julia");
+		mntmJulia.setFont(new Font("Tahoma", Font.PLAIN, 30));
 		mnFractal.add(mntmJulia);
-		mntmJulia.addActionListener(new ActionListener(){
-			public void actionPerformed(ActionEvent e){
+		mntmJulia.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
 				FP.updateImage(F.jArray(numInput));
-				x=2;
+				x = 2;
 			}
 		});
-		
+
 		JMenuItem mntmMandelbrot = new JMenuItem("Mandelbrot");
+		mntmMandelbrot.setFont(new Font("Tahoma", Font.PLAIN, 30));
 		mnFractal.add(mntmMandelbrot);
-		mntmMandelbrot.addActionListener(new ActionListener(){
-			public void actionPerformed(ActionEvent e){
+		mntmMandelbrot.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
 				FP.updateImage(F.maArray(numInput));
-				x=3;
+				x = 3;
 			}
 		});
-		
+
 		JMenuItem mntmMultibrot = new JMenuItem("Multibrot");
+		mntmMultibrot.setFont(new Font("Tahoma", Font.PLAIN, 30));
 		mnFractal.add(mntmMultibrot);
-		mntmMultibrot.addActionListener(new ActionListener(){
-			public void actionPerformed(ActionEvent e){
+		mntmMultibrot.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
 				FP.updateImage(F.muArray(numInput));
-				x=4;
+				x = 4;
 			}
 		});
-		
+
 		JMenu mnColor = new JMenu("Color");
+		mnColor.setFont(new Font("Tahoma", Font.PLAIN, 30));
 		menuBar.add(mnColor);
 		
+				JMenuItem blue = new JMenuItem("Blue");
+				blue.setFont(new Font("Tahoma", Font.PLAIN, 30));
+				mnColor.add(blue);
+				blue.addActionListener(new ActionListener() {
+					public void actionPerformed(ActionEvent e) {
+						FP.setIndexColorModel(newModel.createBluesColorModel(256));
+						if (x == 1) {
+							FP.updateImage(F.bsArray(numInput));
+						}
+						if (x == 2) {
+							FP.updateImage(F.jArray(numInput));
+						}
+						if (x == 3) {
+							FP.updateImage(F.maArray(numInput));
+						}
+						if (x == 4) {
+							FP.updateImage(F.muArray(numInput));
+						}
+					}
+				});
 		
-		JButton mnESC = new JButton("Set escape distance");
-		menuBar.add(mnESC);
-		mnESC.addActionListener(new ActionListener(){
-		
-		
-			public void actionPerformed(ActionEvent e) {
-				
-		
-			
-				try {
-					String[] buttons = { "OK"  };
-					numInput = Double.parseDouble(JOptionPane.showInputDialog("enter a double", "Input"));
-					if(x==1){FP.updateImage(F.bsArray(numInput));}
-					if(x==2){FP.updateImage(F.jArray(numInput));}
-					if(x==3){FP.updateImage(F.maArray(numInput));}
-					if(x==4){FP.updateImage(F.muArray(numInput));}
-					
-				}
-				catch (NumberFormatException n) {
-					 String[] buttons = { "OK"  };
+				JMenuItem gray = new JMenuItem("Gray");
+				gray.setFont(new Font("Tahoma", Font.PLAIN, 30));
+				mnColor.add(gray);
+				gray.addActionListener(new ActionListener() {
+					public void actionPerformed(ActionEvent e) {
+						FP.setIndexColorModel(newModel.createGrayColorModel(256));
+						if (x == 1) {
+							FP.updateImage(F.bsArray(numInput));
+						}
+						if (x == 2) {
+							FP.updateImage(F.jArray(numInput));
+						}
+						if (x == 3) {
+							FP.updateImage(F.maArray(numInput));
+						}
+						if (x == 4) {
+							FP.updateImage(F.muArray(numInput));
+						}
+					}
+				});
 
-					       JOptionPane.showOptionDialog(null, "Please enter a double", "ERROR",
-					        JOptionPane.WARNING_MESSAGE, 0, null, buttons, buttons[0]);
-
-					   
-				}
-				catch (NullPointerException npe){
-					
-				}
-				
-		
-			}
-		
-			});
-		
-       
-		
-		
 		JMenuItem green = new JMenuItem("Green");
+		green.setFont(new Font("Tahoma", Font.PLAIN, 30));
 		mnColor.add(green);
-		green.addActionListener(new ActionListener(){
-			public void actionPerformed(ActionEvent e){
-			FP.setIndexColorModel(newModel.createGreenColorModel(256));
-			if(x==1){FP.updateImage(F.bsArray(numInput));}
-			if(x==2){FP.updateImage(F.jArray(numInput));}
-			if(x==3){FP.updateImage(F.maArray(numInput));}
-			if(x==4){FP.updateImage(F.muArray(numInput));}
+		green.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				FP.setIndexColorModel(newModel.createGreenColorModel(256));
+				if (x == 1) {
+					FP.updateImage(F.bsArray(numInput));
+				}
+				if (x == 2) {
+					FP.updateImage(F.jArray(numInput));
+				}
+				if (x == 3) {
+					FP.updateImage(F.maArray(numInput));
+				}
+				if (x == 4) {
+					FP.updateImage(F.muArray(numInput));
+				}
 			}
 		});
 		
+				JMenuItem purple = new JMenuItem("Purple");
+				purple.setFont(new Font("Tahoma", Font.PLAIN, 30));
+				mnColor.add(purple);
+				purple.addActionListener(new ActionListener() {
+					public void actionPerformed(ActionEvent e) {
+						FP.setIndexColorModel(newModel.createPurpleColorModel(256));
+						if (x == 1) {
+							FP.updateImage(F.bsArray(numInput));
+						}
+						if (x == 2) {
+							FP.updateImage(F.jArray(numInput));
+						}
+						if (x == 3) {
+							FP.updateImage(F.maArray(numInput));
+						}
+						if (x == 4) {
+							FP.updateImage(F.muArray(numInput));
+						}
+					}
+				});
+		
+				JMenuItem rainbow = new JMenuItem("Rainbow");
+				rainbow.setFont(new Font("Tahoma", Font.PLAIN, 30));
+				mnColor.add(rainbow);
+				rainbow.addActionListener(new ActionListener() {
+					public void actionPerformed(ActionEvent e) {
+						FP.setIndexColorModel(newModel.createRainbowColorModel(256));
+						if (x == 1) {
+							FP.updateImage(F.bsArray(numInput));
+						}
+						if (x == 2) {
+							FP.updateImage(F.jArray(numInput));
+						}
+						if (x == 3) {
+							FP.updateImage(F.maArray(numInput));
+						}
+						if (x == 4) {
+							FP.updateImage(F.muArray(numInput));
+						}
+					}
+				});
 
 		JMenuItem red = new JMenuItem("Red");
+		red.setFont(new Font("Tahoma", Font.PLAIN, 30));
 		mnColor.add(red);
-		red.addActionListener(new ActionListener(){
-			public void actionPerformed(ActionEvent e){
-			FP.setIndexColorModel(newModel.createRedColorModel(256));
-			if(x==1){FP.updateImage(F.bsArray(numInput));}
-			if(x==2){FP.updateImage(F.jArray(numInput));}
-			if(x==3){FP.updateImage(F.maArray(numInput));}
-			if(x==4){FP.updateImage(F.muArray(numInput));}
-			
-			}
-		});
-		
+		red.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				FP.setIndexColorModel(newModel.createRedColorModel(256));
+				if (x == 1) {
+					FP.updateImage(F.bsArray(numInput));
+				}
+				if (x == 2) {
+					FP.updateImage(F.jArray(numInput));
+				}
+				if (x == 3) {
+					FP.updateImage(F.maArray(numInput));
+				}
+				if (x == 4) {
+					FP.updateImage(F.muArray(numInput));
+				}
 
-		JMenuItem blue = new JMenuItem("Blue");
-		mnColor.add(blue);
-		blue.addActionListener(new ActionListener(){
-			public void actionPerformed(ActionEvent e){
-			FP.setIndexColorModel(newModel.createBluesColorModel(256));
-			if(x==1){FP.updateImage(F.bsArray(numInput));}
-			if(x==2){FP.updateImage(F.jArray(numInput));}
-			if(x==3){FP.updateImage(F.maArray(numInput));}
-			if(x==4){FP.updateImage(F.muArray(numInput));}
 			}
 		});
-		
 
-		JMenuItem purple = new JMenuItem("Purple");
-		mnColor.add(purple);
-		purple.addActionListener(new ActionListener(){
-			public void actionPerformed(ActionEvent e){
-			FP.setIndexColorModel(newModel.createPurpleColorModel(256));
-			if(x==1){FP.updateImage(F.bsArray(numInput));}
-			if(x==2){FP.updateImage(F.jArray(numInput));}
-			if(x==3){FP.updateImage(F.maArray(numInput));}
-			if(x==4){FP.updateImage(F.muArray(numInput));}
+		JMenuItem turquoise = new JMenuItem("Turquoise");
+		turquoise.setFont(new Font("Tahoma", Font.PLAIN, 30));
+		mnColor.add(turquoise);
+		turquoise.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				FP.setIndexColorModel(newModel.createTurqoiseColorModel(256));
+				if (x == 1) {
+					FP.updateImage(F.bsArray(numInput));
+				}
+				if (x == 2) {
+					FP.updateImage(F.jArray(numInput));
+				}
+				if (x == 3) {
+					FP.updateImage(F.maArray(numInput));
+				}
+				if (x == 4) {
+					FP.updateImage(F.muArray(numInput));
+				}
 			}
 		});
-		
-		JMenuItem turqoise = new JMenuItem("Turqoise");
-		mnColor.add(turqoise);
-		turqoise.addActionListener(new ActionListener(){
-			public void actionPerformed(ActionEvent e){
-			FP.setIndexColorModel(newModel.createTurqoiseColorModel(256));
-			if(x==1){FP.updateImage(F.bsArray(numInput));}
-			if(x==2){FP.updateImage(F.jArray(numInput));}
-			if(x==3){FP.updateImage(F.maArray(numInput));}
-			if(x==4){FP.updateImage(F.muArray(numInput));}
-			}
-		});
-		
-		JMenuItem rainbow = new JMenuItem("Rainbow");
-		mnColor.add(rainbow);
-		rainbow.addActionListener(new ActionListener(){
-			public void actionPerformed(ActionEvent e){
-			FP.setIndexColorModel(newModel.createRainbowColorModel(256));
-			if(x==1){FP.updateImage(F.bsArray(numInput));}
-			if(x==2){FP.updateImage(F.jArray(numInput));}
-			if(x==3){FP.updateImage(F.maArray(numInput));}
-			if(x==4){FP.updateImage(F.muArray(numInput));}
-			}
-		});
-		
-		
-		JMenuItem gray = new JMenuItem("Gray");
-		mnColor.add(gray);
-		gray.addActionListener(new ActionListener(){
-			public void actionPerformed(ActionEvent e){
-			FP.setIndexColorModel(newModel.createGrayColorModel(256));
-			if(x==1){FP.updateImage(F.bsArray(numInput));}
-			if(x==2){FP.updateImage(F.jArray(numInput));}
-			if(x==3){FP.updateImage(F.maArray(numInput));}
-			if(x==4){FP.updateImage(F.muArray(numInput));}
-			}
-		});
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-        
-		
+
 		frame.getContentPane().setLayout(new GridLayout(0, 1, 0, 0));
 		frame.getContentPane().add(FP);
 	}
-	
-
 }
